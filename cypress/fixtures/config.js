ecfa7bd8e1bf1c59ed77e49a19ea77c841830151
artemnih <23387542+artemnih@users.noreply.github.com>
2022-03-23 17:08:05 -0400

    chore: remove more garbage

--- cypress/fixtures/config.js
+++
@@ -1,5 +0,0 @@
-/* Cypress.env('key') calls can be used here 
-Export a config object for testing */
-module.exports = {
-  url: Cypress.env('TEST_URL') || 'http://localhost:4200'
-};
--- cypress/s3/config.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-  BUCKET_NAME: process.env.AWS_BUCKET_NAME,
-  REGION: 'us-east-1',
-  LIMIT: 90
-};
